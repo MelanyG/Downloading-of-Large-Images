83781b748c7b35ecc963027354fc9c504f846ccc
Melany Gulianovych <melanyGulianovych@melanys-mac-mini.local>
2016-03-26 00:43:13 -0700

    Initial Commit

---
+++ Downloading/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Downloading
+//
+//  Created by Melany Gulianovych on 3/26/16.
+//  Copyright © 2016 Melany Gulianovych. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Downloading/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Downloading
+//
+//  Created by Melany Gulianovych on 3/26/16.
+//  Copyright © 2016 Melany Gulianovych. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
